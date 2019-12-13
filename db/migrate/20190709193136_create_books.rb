d5ba72fe7100294d3a86044ae123e35dac1f8a76
Michael Dao <mike@turing.io>
2019-12-13 11:04:51 -0700

    initial commit

---
+++ db/migrate/20190709193136_create_books.rb
@@ -0,0 +1,9 @@
+class CreateBooks < ActiveRecord::Migration[5.1]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.string :publication_year
+      t.integer :pages
+    end
+  end
+end
